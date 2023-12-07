62e01cec9b99bf66dc7d60a0efb28ecb63ae6958
LamSut <116415510+LamSut@users.noreply.github.com>
2023-12-07 08:59:16 +0700

    Delete MooMooBank/Java/OOP directory

--- MooMooBank/Java/OOP/src/Lab4/Bo.java
+++
@@ -1,8 +0,0 @@
-package ThucHanhBuoi4;
-
-import ThucHanhBuoi4.ConVat;
-public class Bo extends ConVat {
-	public void Keu() {
-		System.out.println("Bo keu \"Swim Swim\" ");
-	}
-}
--- MooMooBank/Java/OOP/src/Lab4/SDConVat.java
+++
@@ -1,8 +0,0 @@
-package ThucHanhBuoi4;
-
-public class SDConVat {
-	public static void main(String[] args) {
-		XuanThanh a = new XuanThanh();
-		a.Keu();
-	}
-}
